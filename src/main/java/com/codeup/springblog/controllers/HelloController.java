8cbba5b9ff09eec2eef706435c1e62a74fc81fcf
Justin Welsh <justinwelsh33@gmail.com>
2022-08-02 16:11:40 -0500

    update

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -2,7 +2,7 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*; //imports ALL annotations
+import org.springframework.web.bind.annotation.*; //'.*' imports ALL annotations
 
 @Controller
 public class HelloController {
